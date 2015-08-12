70333fe968e588fc6768dc46f19d98cc44668eff
lolliesoft <starplush@gmail.com>
2015-08-12 14:47:23 -0400

    adding custom DB functionality

---
+++ bartenderexpressReloaded/customRecipes.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace bartenderexpressReloaded
+{
+    partial class customRecipes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DrinkName = new System.Windows.Forms.TextBox();
+            this.Amt1 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DrinkName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Amount 1:";
+            // 
+            // DrinkName
+            // 
+            this.DrinkName.Location = new System.Drawing.Point(90, 10);
+            this.DrinkName.Name = "DrinkName";
+            this.DrinkName.Size = new System.Drawing.Size(161, 20);
+            this.DrinkName.TabIndex = 2;
+            // 
+            // Amt1
+            // 
+            this.Amt1.Location = new System.Drawing.Point(90, 38);
+            this.Amt1.Name = "Amt1";
+            this.Amt1.Size = new System.Drawing.Size(161, 20);
+            this.Amt1.TabIndex = 3;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(181, 118);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 4;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // customRecipes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Amt1);
+            this.Controls.Add(this.DrinkName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "customRecipes";
+            this.Text = "customRecipes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DrinkName;
+        private System.Windows.Forms.TextBox Amt1;
+        private System.Windows.Forms.Button Save;
+    }
+}
---
+++ bartenderexpressReloaded/customRecipes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace bartenderexpressReloaded
+{
+    public partial class customRecipes : Form
+    {
+        string connectionString;
+
+        public customRecipes()
+        {
+            InitializeComponent();
+            connectionString = @"Data Source = |DataDirectory|\\XpressShots.db";    
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn10 = new SQLiteConnection(connectionString))
+            {
+                conn10.Open();
+                SQLiteCommand cmd = new SQLiteCommand();
+                cmd.CommandText = @"INSERT into myRecipes (DrinkName, Amt1) Values (@DrinkName,@Amt1) ";
+                cmd.Connection = conn10;
+                cmd.Parameters.Add(new SQLiteParameter())
+
+            }
+
+        }
+    }
+}
