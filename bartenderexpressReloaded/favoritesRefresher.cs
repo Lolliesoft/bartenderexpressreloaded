5623ee8921132ff10628c771a00fa02918bef9da
lolliesoft <starplush@gmail.com>
2015-11-06 14:49:34 -0500

    Adding class to update favorites boolean values in drinks table

---
+++ bartenderexpressReloaded/favoritesRefresher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+
+namespace bartenderexpressReloaded
+{
+    class favoritesRefresher
+    {
+        public void RefreshFavs()
+        {
+            using (SQLiteConnection cs101 = new SQLiteConnection("Data Source = |DataDirectory|\\Custom.db"))
+            {
+
+                SQLiteCommand cmd101 = new SQLiteCommand("SELECT * FROM Favorites", cs101);
+                SQLiteDataReader reader = cmd101.ExecuteReader();
+                if (reader.Read())
+                {
+                    using (SQLiteConnection cs102 = new SQLiteConnection("Data Source = |DataDirectory|\\XpressShots.db"))
+                    {
+                        // need to loop through array
+                        SQLiteCommand cmd102 = new SQLiteCommand("UPDATE drinks  SET favorite = 1 WHERE name ='" + " rdring[*].ToString()", cs102);
+                        cs102.Open();
+                        cmd102.ExecuteNonQuery();
+                        cs102.Close();
+                    }
+
+                }
+            }
+        }
+    }
+}
