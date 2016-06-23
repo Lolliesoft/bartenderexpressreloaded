d9a30f853540a476ba46be267041fc5363fef205
PCH-BurneyA <PCH-BurneyA@AH009922-VM4>
2016-06-23 11:04:18 -0400

    fixed fuinctionally of adding custom recipe into favorites

--- bartenderexpressReloaded/Form3.cs
+++ bartenderexpressReloaded/Form3.cs
@@ -90,7 +90,7 @@ namespace bartenderexpressReloaded
 
             if (FavoritesButton.Text == "myRecipes")
             {
-                using (SQLiteConnection cs228 = new SQLiteConnection("Data Source = |DataDirectory|\\Custom.db"))
+                using (SQLiteConnection cs228 = new SQLiteConnection("Data Source = |DataDirectory|\\XpressShots.db"))
                 {
                     cs228.Open();
 
@@ -98,70 +98,78 @@ namespace bartenderexpressReloaded
                     {
                         string FavoritesTablekey = FavoritesTable + "_key";
 
-                        //SQLiteCommand cmd = new SQLiteCommand("SELECT " + FavoritesTablekey + " FROM " + FavoritesTable + " WHERE name ='" + (FavoriteName) + "'", cs228);
-                        // Gets name and amounts 
-
-                        SQLiteCommand cmd = new SQLiteCommand("SELECT name, amt1, amt2, amt3, amt4, amt5, amt6, amt7, amt8, amt9, amt10, amt11, amt12, amt13, amt14, amt15, directions,ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15 FROM " + FavoritesTable + " WHERE " + FavoritesTablekey + " IN (Select " + FavoritesTablekey + " from " + FavoritesTable + " WHERE name = '" + FavoriteName + "')", cs228);
-
+                        SQLiteCommand cmd = new SQLiteCommand("SELECT " + FavoritesTablekey + " FROM " + FavoritesTable + " WHERE name ='" + (FavoriteName) + "'", cs228);
                         SQLiteDataReader rdr1 = cmd.ExecuteReader();
 
                         if (rdr1.Read())
                         {
-                           
-                           // MessageBox.Show(rdr1[0].ToString());
 
-                            
-                                SQLiteCommand cmd2 = cs228.CreateCommand();
-
-                                cmd2.CommandText = @"INSERT into Favorites (name, amt1, amt2, amt3, amt4, amt5, amt6, amt7, amt8, amt9, amt10, amt11, amt12, amt13, amt14, amt15, directions, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9,ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15) 
-                                 Values (@DrinkName,@Amt1,@Amt2,@Amt3,@Amt4,@Amt5,@Amt6,@Amt7,@Amt8,@Amt9,@Amt10,@Amt11,@Amt12,@Amt13,@Amt14,@Amt15,@Directions,
-                                     @Ing1,@Ing2,@Ing3,@Ing4,@Ing5,@Ing6,@Ing7,@Ing8,@Ing9,@Ing10,@Ing11,@Ing12,@Ing13,@Ing14,@Ing15)";
+                            //MessageBox.Show(rdr1["FavoritesTablekey"].ToString());
 
 
-                                cmd2.Parameters.Add(new SQLiteParameter("@DrinkName", rdr1[0]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt1", rdr1[1]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt2", rdr1[2]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt3", rdr1[3]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt4", rdr1[4]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt5", rdr1[5]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt6", rdr1[6]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt7", rdr1[7]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt8", rdr1[8]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt9", rdr1[9]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt10", rdr1[10]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt11", rdr1[11]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt12", rdr1[12]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt13", rdr1[13]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt14", rdr1[14]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Amt15", rdr1[15]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Directions", rdr1[16]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing1", rdr1[17]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing2", rdr1[18]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing3", rdr1[19]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing4", rdr1[20]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing5", rdr1[21]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing6", rdr1[22]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing7", rdr1[23]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing8", rdr1[24]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing9", rdr1[25]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing10", rdr1[26]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing11", rdr1[27]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing12", rdr1[28]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing13", rdr1[29]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing14", rdr1[30]));
-                                cmd2.Parameters.Add(new SQLiteParameter("@Ing15", rdr1[31]));
+                            SQLiteCommand cmd1 = new SQLiteCommand("SELECT name, amt1, amt2, amt3, amt4, amt5, amt6, amt7, amt8, amt9, amt10, amt11, amt12, amt13, amt14, amt15, directions," +
+                                                                    "ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9," +
+                                                                "ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15 FROM " + FavoritesTable + " WHERE " + FavoritesTablekey + "=" + (rdr1[FavoritesTablekey]) + "", cs228);
+                            SQLiteDataReader rdring = cmd1.ExecuteReader();
 
+                            using (SQLiteConnection cs240 = new SQLiteConnection("Data Source = |DataDirectory|\\Custom.db"))
+                                if (rdring.Read())
+                                {
+                                    //MessageBox.Show(rdring[0].ToString());
+                                    SQLiteCommand cmd2 = new SQLiteCommand();
+                                    cmd2.CommandText = @"INSERT into Favorites (name, amt1, amt2, amt3, amt4, amt5, amt6, amt7, amt8, amt9, amt10, amt11, amt12, amt13, amt14, amt15, directions,
+                                                            ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9,
+                                                            ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15) 
+                                     Values (@DrinkName,@Amt1,@Amt2,@Amt3,@Amt4,@Amt5,@Amt6,@Amt7,@Amt8,@Amt9,@Amt10,@Amt11,@Amt12,@Amt13,@Amt14,@Amt15,@Directions,
+                                                            @Ing1,@Ing2,@Ing3,@Ing4,@Ing5,@Ing6,@Ing7,@Ing8,@Ing9,@Ing10,@Ing11,@Ing12,@Ing13,@Ing14,@Ing15)";
 
-                                cmd2.Prepare();
-                                cmd2.ExecuteNonQuery();
-                                cs228.Close();
-                            
+                                    cmd2.Connection = cs240;
+                                    //string DrinkName = rdring[0].ToString();
+                                    cmd2.Parameters.Add(new SQLiteParameter("@DrinkName", rdring[0]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt1", rdring[1]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt2", rdring[2]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt3", rdring[3]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt4", rdring[4]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt5", rdring[5]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt6", rdring[6]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt7", rdring[7]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt8", rdring[8]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt9", rdring[9]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt10", rdring[10]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt11", rdring[11]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt12", rdring[12]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt13", rdring[13]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt14", rdring[14]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Amt15", rdring[15]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Directions", rdring[16]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing1", rdring[17]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing2", rdring[18]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing3", rdring[19]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing4", rdring[20]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing5", rdring[21]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing6", rdring[22]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing7", rdring[23]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing8", rdring[24]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing9", rdring[25]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing10", rdring[26]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing11", rdring[27]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing12", rdring[28]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing13", rdring[29]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing14", rdring[30]));
+                                    cmd2.Parameters.Add(new SQLiteParameter("@Ing15", rdring[31]));
+
+                                    cs240.Open();
+                                    cmd2.ExecuteNonQuery();
+                                    cs240.Close();
+                                    cs228.Close();
                                 }
-                             }   
-                            }
                         }
-            else 
-                {
+                    }
+                }
+            }
+
+            else
+            {
                 using (SQLiteConnection cs221 = new SQLiteConnection("Data Source = |DataDirectory|\\XpressShots.db"))
                 {
                     cs221.Open();
@@ -676,7 +684,8 @@ namespace bartenderexpressReloaded
                     }
                 }
             }
-        }
+                }
+            
         private void tileItem1_ItemClick(object sender, DevExpress.XtraEditors.TileItemEventArgs e)
         {
             System.Diagnostics.Process.Start("http://www.lolliesoft.com");
--- bartenderexpressReloaded/customRecipes.cs
+++ bartenderexpressReloaded/customRecipes.cs
@@ -34,11 +34,13 @@ namespace bartenderexpressReloaded
     {
 
         string connectionString;
+        string connectionString2;
 
         public customRecipes()
         {
             InitializeComponent();
             connectionString = @"Data Source = |DataDirectory|\\Custom.db";
+            connectionString2 = @"Data Source = |DataDirectory|\\XpressShots.db";
 
         }
 
@@ -99,6 +101,62 @@ namespace bartenderexpressReloaded
 
             }
 
+            //copy to xpress
+
+            using (SQLiteConnection conn1000 = new SQLiteConnection(connectionString2))
+            {
+
+
+                SQLiteCommand cmd = new SQLiteCommand();
+                cmd.CommandText = @"INSERT into myRecipes (name, amt1, amt2, amt3, amt4, amt5, amt6, amt7, amt8, amt9, amt10, amt11, amt12, amt13, amt14, amt15, directions,
+                                                            ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9,
+                                                            ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15) 
+                Values (@DrinkName,@Amt1,@Amt2,@Amt3,@Amt4,@Amt5,@Amt6,@Amt7,@Amt8,@Amt9,@Amt10,@Amt11,@Amt12,@Amt13,@Amt14,@Amt15,@Directions,
+                                                            @Ing1,@Ing2,@Ing3,@Ing4,@Ing5,@Ing6,@Ing7,@Ing8,@Ing9,@Ing10,@Ing11,@Ing12,@Ing13,@Ing14,@Ing15)";
+                cmd.Connection = conn1000;
+                cmd.Parameters.Add(new SQLiteParameter("@DrinkName", txtDrinkName.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt1", txtAmt1.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt2", txtAmt2.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt3", txtAmt3.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt4", txtAmt4.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt5", txtAmt5.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt6", txtAmt6.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt7", txtAmt7.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt8", txtAmt8.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt9", txtAmt9.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt10", txtAmt10.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt11", txtAmt11.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt12", txtAmt12.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt13", txtAmt13.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt14", txtAmt14.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Amt15", txtAmt15.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@directions", txtDirections.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing1", Ing1.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing2", Ing2.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing3", Ing3.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing4", Ing4.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing5", Ing5.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing6", Ing6.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing7", Ing7.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing8", Ing8.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing9", Ing9.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing10", Ing10.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing11", Ing11.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing12", Ing12.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing13", Ing13.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing14", Ing14.Text));
+                cmd.Parameters.Add(new SQLiteParameter("@Ing15", Ing15.Text));
+
+
+                conn1000.Open();
+                cmd.ExecuteNonQuery();
+
+                conn1000.Close();
+
+                //MessageBox.Show("Custom Recipe Saved", "MyRecipe Added", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //Refresh database
+            }
+
+            }
         }
-    }
 }
